88c915a0a954ff017e15343fc23d4807635f2f3a
loup <hugo.molle@yahoo.fr>
2024-11-18 11:19:19 -0800

    fixed bad event handling

--- esp32/wifi6/include/my_net_tools.c
+++ esp32/wifi6/include/my_net_tools.c
@@ -53,8 +53,10 @@
 #define DEFAULT_SCAN_LIST_SIZE 10
 
 void ip_forwarding();
+void start_ping();
 
-static void wifi_station_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+//static 
+void wifi_station_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
         printf("esp_wifi_connect : %i\n",esp_wifi_connect());
     } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
@@ -67,8 +69,34 @@ static void wifi_station_event_handler(void* arg, esp_event_base_t event_base, i
         ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
         printf("Connected! Got IP: " IPSTR "\n", IP2STR(&event->ip_info.ip));
         // Now you can send packets, as the IP address is assigned
+
+        // try the ping before nat
+        // start_ping();
+
         // setup nat
         ip_forwarding(event->ip_info.ip);
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_CONNECTED) {
+        printf("Connected!");
+
+        ip_event_got_ip_t* ip_event_data = (ip_event_got_ip_t*) event_data;
+
+        printf("start ip forwarding\n");
+        ip_forwarding(ip_event_data->ip_info.ip);
+
+    }
+}
+
+void wifi_sta_ap_event_ip_forwarding(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+{
+    printf("ip forwarding event\n");
+
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_AP_STACONNECTED) {
+        printf("event ap sta connected!\n");
+
+        ip_event_got_ip_t* ip_event_data = (ip_event_got_ip_t*) event_data;
+
+        ip_forwarding(ip_event_data->ip_info.ip);
+
     }
 }
 
@@ -179,7 +207,17 @@ void init_AP_STA()
 {
     // first the sta to connect to provider AP
     esp_netif_init();
-    esp_event_loop_create_default();
+    
+    // esp_err_t crel_err = esp_event_loop_create_default();
+    // if(crel_err != ESP_OK)
+    // {
+    //     printf("failed to create event loop");
+    // }
+
+    // add event handler for wifi and ip address change
+    // esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_station_event_handler, NULL);
+    // esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_station_event_handler, NULL);
+    // esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_CONNECTED, &wifi_station_event_handler, NULL);
 
     esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
     assert(sta_netif);
@@ -187,10 +225,6 @@ void init_AP_STA()
     // STA
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     esp_wifi_init(&cfg);
-
-    // add event handler for wifi and ip address change
-    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_station_event_handler, NULL);
-    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_station_event_handler, NULL);
 
     esp_netif_set_hostname(sta_netif, "STA_HANDLE");
 
@@ -232,13 +266,19 @@ void init_AP_STA()
 
 }
 
+static const char *NAPT = "ip_forwarding";
 
 void ip_forwarding(struct esp_ip4_addr p_addr) // or nat ???
 {
-    printf("ip_forwarding yo\n");
+    printf("ip_forwarding\n");
     // initial aproach ->> ip_napt_forward()
 
+    ESP_LOGI(NAPT, "enable ip_napt for address " IPSTR, IP2STR(&p_addr));
+
     ip_napt_enable(p_addr.addr, 1); // doesn't work lol
+
+    // printf("ip_napt_enabled for address %i\n", IP2STR(&p_addr.addr));
+    ESP_LOGI(NAPT, "ip_napt_enabled for address " IPSTR, IP2STR(&p_addr));
 
     // esp_netif_t * netif_sta = esp_netif_get_handle_from_ifkey("STA_HANDLE");
 
@@ -255,7 +295,97 @@ void ip_forwarding(struct esp_ip4_addr p_addr) // or nat ???
 
 }
 
+#include "ping/ping_sock.h"
+
+static const char *TAG = "8.8.8.8";
+
+// Callback function to handle ping results
+static void ping_results(esp_ping_handle_t *pf, void *args) {
+    uint32_t elapsed_time;
+    ip4_addr_t target_addr;
+    uint8_t ttl;
+
+    esp_log_level_set("wifi", ESP_LOG_DEBUG);
+    esp_log_level_set("ping", ESP_LOG_DEBUG);
+    
+    printf("start ping results\n");
+    // Get the ping response details
+    // Check TIMEGAP (elapsed time)
+    if (esp_ping_get_profile(*pf, ESP_PING_PROF_TIMEGAP, &elapsed_time, sizeof(elapsed_time)) == ESP_OK) {
+        ESP_LOGI(TAG, "Ping time=%d ms", (int)elapsed_time);
+    } else {
+        ESP_LOGE(TAG, "Failed to retrieve ESP_PING_PROF_TIMEGAP");
+    }
+
+    // Check IPADDR (target address)
+    if (esp_ping_get_profile(*pf, ESP_PING_PROF_IPADDR, &target_addr, sizeof(target_addr)) == ESP_OK) {
+        ESP_LOGI(TAG, "Ping target IP=" IPSTR, IP2STR(&target_addr));
+    } else {
+        ESP_LOGE(TAG, "Failed to retrieve ESP_PING_PROF_IPADDR");
+    }
+
+    // Check TTL
+    if (esp_ping_get_profile(*pf, ESP_PING_PROF_TTL, &ttl, sizeof(ttl)) == ESP_OK) {
+        ESP_LOGI(TAG, "Ping TTL=%d", ttl);
+    } else {
+        ESP_LOGE(TAG, "Failed to retrieve ESP_PING_PROF_TTL");
+    }
+
+
+    ESP_LOGI(TAG, "Ping reply from " IPSTR ": time=%u ms, ttl=%u",
+             IP2STR(&target_addr), (int)elapsed_time, (int)ttl);
+}
+
+void start_ping() {
+    // Set up ping target
+    ip_addr_t target_addr;
+    ipaddr_aton("8.8.8.8", &target_addr);
+
+    // Configure the ping parameters
+    esp_ping_config_t ping_config = ESP_PING_DEFAULT_CONFIG();
+    ping_config.target_addr = target_addr;
+    ping_config.count = 4;  // Number of ping attempts
+
+    // Set up callback for ping response
+    esp_ping_callbacks_t cbs = {
+        .cb_args = NULL,
+        .on_ping_success = ping_results,  // Called on each successful ping
+        .on_ping_end = ping_results,      // Called when pinging ends
+    };
+
+    esp_ping_handle_t ping;
+    esp_err_t err = esp_ping_new_session(&ping_config, &cbs, &ping);
+    if (err == ESP_OK) {
+        ESP_LOGE(TAG, "good new ping session");
+        esp_err_t start_err = esp_ping_start(ping);
+        if(start_err == ESP_OK) {
+            ESP_LOGE(TAG, "esp ping start OK");
+        }
+    } else {
+        ESP_LOGE(TAG, "Failed to start ping session");
+    }
+}
+
+
 // void send_ping_to_host(const char *target_ip)
 // {
 //     printf("send_ping_to_host\n");
+
+//     esp_ping_config_t ping_config = ESP_PING_DEFAULT_CONFIG();
+//     ping_config.target_addr = inet_addr("8.8.8.8");  // Set the target IP address
+//     ping_config.count = 4;                           // Number of ping requests to send
+
+//     esp_ping_callbacks_t cbs = {
+//         .cb_args = NULL,
+//         .on_ping_success = ping_results,             // Called on each ping success
+//         .on_ping_end = ping_results,                 // Called when pinging ends
+//     };
+
+//     esp_ping_handle_t ping;
+//     esp_err_t err = esp_ping_new_session(&ping_config, &cbs, &ping);
+//     if (err == ESP_OK) {
+//         esp_ping_start(ping);
+//     } else {
+//         ESP_LOGE(TAG, "Failed to start ping session");
+//     }
 // }
--- esp32/wifi6/src/main.c
+++ esp32/wifi6/src/main.c
@@ -13,23 +13,55 @@
 
 #define DEFAULT_SCAN_LIST_SIZE 10
 
-void app_main()
+void init_nvs()
 {
-
-    printf("start :\n");
-
-    //----------------------------------------------------------------
-
-    // //  Initialize NVS
+    //  Initialize NVS
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         ESP_ERROR_CHECK(nvs_flash_erase());
         ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
+}
+
+void got_ip()
+{
+    printf("event got ip\n");
+}
+
+void app_main()
+{
+
+    init_nvs();
+
+    esp_err_t crel_err = esp_event_loop_create_default();
+    if(crel_err != ESP_OK)
+    {
+        printf("failed to create event loop");
+    }
+
+    printf("start wifi :\n");
+    esp_log_level_set("wifi", ESP_LOG_DEBUG);
+
+    // event handlers
+    esp_event_handler_instance_register(IP_EVENT_STA_GOT_IP, ESP_EVENT_ANY_ID, &got_ip, NULL, NULL);
+    
+    esp_err_t hre = esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_AP_STACONNECTED, &wifi_sta_ap_event_ip_forwarding, NULL);
+    if (hre != ESP_OK)
+    {
+        printf("Failed to register event handler\n");
+    }
+
+
+    // Be a AP_STA with ip forwarding
+    init_AP_STA();
+
 
     //----------------------------------------------------------------
-    esp_log_level_set("wifi", ESP_LOG_DEBUG);
+
+
+
+    //----------------------------------------------------------------
     
     // Be a STA
     // init_station();
@@ -40,7 +72,8 @@ void app_main()
 
 
     // Be a AP_STA with ip forwarding
-    init_AP_STA();
+    // init_AP_STA();
 
+    // start_ping();
     // ip_forwarding();
 }
