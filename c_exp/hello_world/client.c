6709f54af345b82ad5fc7f56497acd87fa20a24c
loup <hugo.molle@yahoo.fr>
2024-12-11 13:54:29 -0800

    add new c client/server for esp32 tests

---
+++ c_exp/hello_world/client.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("hello world\n");
+
+	// setup client
+	//
+	return 0;
+}
---
+++ c_exp/hello_world/server.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("start server\n");
+
+	return 0;
+}
